764b2d433678dfeeb6f76e63f6717ec6f722041b
talkei <rafael.marcolin@ftd.com.br>
2019-05-08 00:19:46 -0300

    exercicios

--- 7.14- Exercise/exercise/index.js
+++ 7.14- Exercise/exercise/index.js
@@ -20,7 +20,7 @@ async function getCourses() {
     return await Course
         .find({ isPublished: true, tags: /backend/i })
         .sort({ name: 1 })
-        .select({ name: 1, author: 1 })
+        .select({ name: 1, author: 1, isPublished: 1 })
 }
 
 async function getCoursesbyPrice() {
@@ -28,14 +28,42 @@ async function getCoursesbyPrice() {
         // .find({ 'isPublished': true, 'tags':  { $in: [/backend/i, /frontend/i] }  })
         // alternativa
         .find({ 'isPublished': true })
-        .or([{ tags: /frontend/i }, { tags: /backend/i} ])
-        .sort ('-price')
+        .or([{ tags: /frontend/i }, { tags: /backend/i }])
+        .sort('-price')
         .select('name author price')
 }
+
+async function getCoursesbyPriceandName() {
+    return await Course
+        // .find({ 'isPublished': true, 'tags':  { $in: [/backend/i, /frontend/i] }  })
+        // alternativa
+.find({ 'isPublished': true })
+        .or([
+            { price:{ $gte: 15 } },
+            { name: /.*by*./i } 
+        ])
+        .sort('-price')
+        .select('name author price')
+}
+
+async function updateCourseQuery (id) {
+    const course = await Course.findById(id);
+
+    if (!course) {
+        console.log('Curso inexistente')
+        return
+    };
+    course.isPublished = true;
+    course.author = 'Rafael K M';
+
+    const result = await course.save();
+    console.log(result);
+}
 async function run() {
-    // const courses = await getCourses();
-    const courses = await getCoursesbyPrice();
+    const courses = await getCourses();
+    // const courses = await getCoursesbyPriceandName();
     console.log(courses);
 }
 
+updateCourseQuery('5a68fdf95db93f6477053ddd');
 run();
