256c3ef6179fdf0a18062017a3328ffc9a8bfaff
kuvasney-ftd <rafael.marcolin@ftd.com.br>
2019-05-13 20:19:04 -0300

    movies

--- vidly/routes/customers.js
+++ vidly/routes/customers.js
@@ -1,7 +1,7 @@
-const { Customer, validate } = require('../models/customer');
+const {Customer, validate} = require('../models/customer'); 
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 
 router.get('/', async (req, res) => {
   const customers = await Customer.find().sort('name');
@@ -12,35 +12,35 @@ router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  let customer = new Customer({
-      name: req.body.name,
-      isGold: req.body.isGold,
-      phone: req.body.phone
+  let customer = new Customer({ 
+    name: req.body.name,
+    isGold: req.body.isGold,
+    phone: req.body.phone
   });
   customer = await customer.save();
+  
   res.send(customer);
 });
 
 router.put('/:id', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
-  
-  let customer = await Customer.findOneAndUpdate(req.params.id, { 
+
+  const customer = await Customer.findByIdAndUpdate(req.params.id,
+    { 
       name: req.body.name,
       isGold: req.body.isGold,
       phone: req.body.phone
-    }, {
-    new: true
-  });
-  
-  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+    }, { new: true });
 
+  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+  
   res.send(customer);
 });
 
 router.delete('/:id', async (req, res) => {
-  const customer = await Customer.deleteOne({ _id: req.params.id })
-  
+  const customer = await Customer.findByIdAndRemove(req.params.id);
+
   if (!customer) return res.status(404).send('The customer with the given ID was not found.');
 
   res.send(customer);
@@ -48,10 +48,10 @@ router.delete('/:id', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   const customer = await Customer.findById(req.params.id);
-  
+
   if (!customer) return res.status(404).send('The customer with the given ID was not found.');
 
   res.send(customer);
 });
 
-module.exports = router;
+module.exports = router; 
--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,7 +1,7 @@
+const {Genre, validate} = require('../models/genre');
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const { Genre, validate } = require('../models/genre');
 
 router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name');
@@ -12,29 +12,28 @@ router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  let genre = new Genre({
-    name: req.body.name
-  });
+  let genre = new Genre({ name: req.body.name });
   genre = await genre.save();
+  
   res.send(genre);
 });
 
 router.put('/:id', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
-  
-  let genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+
+  const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
     new: true
   });
-  
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+  
   res.send(genre);
 });
 
 router.delete('/:id', async (req, res) => {
-  const genre = await Genre.findByIdAndRemove(req.params.id)
-  
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
   res.send(genre);
@@ -42,7 +41,7 @@ router.delete('/:id', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   const genre = await Genre.findById(req.params.id);
-  
+
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
   res.send(genre);
